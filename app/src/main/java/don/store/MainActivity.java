7a9b8c420ff86564d8acf5b1c119bb23d4b4b860
adrain <adrain1208@163.com>
2019-04-11 01:40:35 +0800

    1. retrofit

--- app/src/main/java/don/store/MainActivity.java
+++ app/src/main/java/don/store/MainActivity.java
@@ -1,14 +1,40 @@
 package don.store;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends Activity {
+
+    private GitHubService service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Retrofit retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://api.github.com/").build();
+        service = retrofit.create(GitHubService.class);
+        Call call = service.listRepos("octocat");
+        call.enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                System.out.println(response.body().get(0).getArchive_url());
+            }
+
+
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                System.out.println(t);
+            }
+        });
     }
 }
